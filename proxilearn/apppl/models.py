f98307fb7dda598b3c4da7d01a5b60c14e94c131
Nathan Lufuluabo <lufuluabon@outlook.fr>
2024-11-28 16:35:35 +0100

    Corrected database

--- proxilearn/apppl/models.py
+++ proxilearn/apppl/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django_enum import EnumField
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Node(models.Model):
@@ -8,7 +8,7 @@ class Node(models.Model):
     category = models.IntegerField()
     
     def __str__(self):
-        return f"Catégorie : {category} ; Difficulté : {difficulty}"
+        return f"Catégorie : {self.category} ; Difficulté : {self.difficulty}"
 
 class Student(models.Model):
     id = models.AutoField(primary_key=True)
@@ -16,33 +16,33 @@ class Student(models.Model):
     surname = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{name} {surname}"
+        return f"{self.name} {self.surname}"
 
 class Exercice(models.Model):
 
     class State(models.TextChoices):
             SUCCEED = 'S', _('SUCCEED') 
             FAILED = 'F', _('FAILED')
-            AVAILABLE = 'A', _('ACCESSIBLE')
+            AVAILABLE = 'A', _('AVAILABLE')
             UNAVAILABLE = 'U',_('UNAVAILABLE')
 
     id = models.AutoField(primary_key=True)
     state = models.CharField(max_length=2, choices=State, default=State.UNAVAILABLE)
-    node_id = models.IntegerField()
-    student_id = models.IntegerField()
+    node_id = models.ForeignKey(Node, on_delete=models.PROTECT, blank=False, null=False, related_name='exercices')
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE, blank=False, null=False, related_name='exercices')
     r_score = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return f"Exo{self.id} ==> {self.state}"
 
 class Trial(models.Model):
     id = models.AutoField(primary_key=True)
-    exercice_id = models.IntegerField()
-    date = models.models.DateField(_(""), auto_now=False, auto_now_add=False)
-    question = models.TextField()
-    solution = models.TextField()
-    student_answer = models.TextField()
-    distance = models.TextField()
+    exercice_id = models.ForeignKey(Exercice, on_delete=models.CASCADE, blank=False, null=False, related_name='trials')
+    date = models.DateField(_(""), auto_now=False, auto_now_add=True)
+    question = models.CharField(max_length=255, blank=False, null=False)
+    solution = models.CharField(max_length=255, blank=False, null=False)
+    student_answer = models.CharField(max_length=255, blank=False, null=False)
+    distance = models.IntegerField()
 
     def __str__(self):
-        return self.id
+        return f"{self.question} ; distance={self.distance}"
